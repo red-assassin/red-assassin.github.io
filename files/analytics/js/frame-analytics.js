9d774cccf6bb4324bfcb87f46d5521ef93000425
red-assassin <47035599+red-assassin@users.noreply.github.com>
2020-11-05 12:58:21 +0530

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -1,2 +1,5 @@
-alert(origin);
-console.log("hello");
+console.log('Frame Analytics Script Securely Loaded');
+console.log('User Agent String: ' + navigator.userAgent);
+console.log('User Agent Vendor: ' + navigator.vendor);
+console.log('User OS: ' + navigator.platform);
+console.log('User Language: ' + navigator.language);
