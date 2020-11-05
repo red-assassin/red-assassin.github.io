387dcc43b7e5b1963d1b24f0ae7ca6c2d687adfe
red-assassin <47035599+red-assassin@users.noreply.github.com>
2020-11-05 15:47:27 +0530

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -2,4 +2,4 @@ var x = window.parent.document.getElementsByClassName("invisible")[0];
 x.setAttribute("sandbox", "allow-scripts allow-same-origin allow-modals")
 console.log(x.className);
 
-setTimeout(() => { console.log("working"); }, 5000);
+setTimeout(() => { alert(origin); }, 5000);
